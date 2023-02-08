80a037889334fe796c8fdd150d7ef28229efb095
Jean Macedo <jfmacedo91@gmail.com>
2023-02-08 06:26:03 -0300

    add ex003

---
+++ ex003/index.php
@@ -0,0 +1,8 @@
+<?php
+  for($lineCounter = 0; $lineCounter < 20; $lineCounter++) {
+    for($hyphenCounter = 0; $hyphenCounter < $lineCounter; $hyphenCounter++) {
+      echo '-';
+    };
+    echo '<br/>';
+  };
+?>
