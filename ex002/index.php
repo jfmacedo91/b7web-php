9b950feec1e460a6deb1cca62622ee58f364c196
Jean Macedo <jfmacedo91@gmail.com>
2023-02-08 06:19:21 -0300

    add ex002

---
+++ ex002/index.php
@@ -0,0 +1,8 @@
+<?php
+  for($lineCounter = 0; $lineCounter < 10; $lineCounter++) {
+    for($hyphenCounter = 0; $hyphenCounter < 10; $hyphenCounter++) {
+      echo '-';
+    };
+    echo '<br/>';
+  };
+?>
