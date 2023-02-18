330aa4913c2a6a0bbef6f2460bac36121848524f
Jean Macedo <jfmacedo91@gmail.com>
2023-02-18 02:01:30 -0300

    add database class 006

---
+++ database/006/index.php
@@ -0,0 +1,28 @@
+<?php
+  $file = 'anya.jpg';
+  $maxWidth = 500;
+  $maxHeight = 250;
+
+  list($originalWidth, $originalHeight) = getimagesize($file);
+
+  $originalRatio = $originalWidth / $originalHeight;
+  $maxRatio = $maxWidth / $maxHeight;
+
+  $finalWidth = 0;
+  $finalHeight = 0;
+
+  if($originalRatio < $maxRatio) {
+    $finalWidth = $maxHeight * $originalRatio;
+    $finalHeight = $maxHeight;
+  } else {
+    $finalHeight = $maxWidth / $originalRatio;
+    $finalWidth = $maxWidth;
+  }
+
+  $image = imagecreatetruecolor($finalWidth, $finalHeight);
+  $originalImg = imagecreatefromjpeg($file);
+
+  imagecopyresampled($image, $originalImg, 0, 0, 0, 0, $finalWidth, $finalHeight, $originalWidth, $originalHeight);
+
+  imagejpeg($image, 'Anya'.$finalHeight.'px.jpg', 100);
+?>
