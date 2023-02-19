e76f9865029021dd054903b10d0616d7795466d5
Jean Macedo <jfmacedo91@gmail.com>
2023-02-19 08:07:30 -0300

    add dynamic view

--- mvc/src/controllers/HomeController.php
+++ mvc/src/controllers/HomeController.php
@@ -5,24 +5,36 @@ use \core\Controller;
 
 class HomeController extends Controller {
 
-    public function index() {
-        $this->render('home', ['nome' => 'Jean Fernandes de Macedo']);
-    }
+	public function index() {
+		$nome = 'Jean';
+		$posts = [
+			['titulo' => 'Titulo de teste 1', 'corpo' => 'Corpo de teste 1'],
+			['titulo' => 'Titulo de teste 2', 'corpo' => 'Corpo de teste 2'],
+			['titulo' => 'Titulo de teste 3', 'corpo' => 'Corpo de teste 3'],
+			['titulo' => 'Titulo de teste 4', 'corpo' => 'Corpo de teste 4'],
+			['titulo' => 'Titulo de teste 5', 'corpo' => 'Corpo de teste 5']
+		];
+		$this->render('home', [
+			'nome' => $nome,
+			'idade' => 31,
+			'posts' => $posts
+		]);
+	}
 
-    public function fotos() {
-        $this->render('fotos');
-    }
+	public function fotos() {
+		$this->render('fotos');
+	}
 
-    public function foto($parametros) {
-        echo 'Foto '.$parametros['id'];
-    }
+	public function foto($parametros) {
+		echo 'Foto '.$parametros['id'];
+	}
 
-    public function sobre() {
-        $this->render('sobre');
-    }
+	public function sobre() {
+		$this->render('sobre');
+	}
 
-    public function sobreP($args) {
-        print_r($args);
-    }
+	public function sobreP($args) {
+		print_r($args);
+	}
 
 }
--- mvc/src/views/pages/home.php
+++ mvc/src/views/pages/home.php
@@ -1,3 +1,9 @@
 <?php $render('header'); ?>
 
-Opa, <?=$nome;?>
+Meu nome é <?=$nome;?>, tenho <?=$idade;?>
+<hr>
+
+<?php foreach($posts as $post): ?>
+  <h3><?php echo $post['titulo']; ?></h3>
+  <p><?php echo $post['corpo']; ?></p>
+<?php endforeach ?>
